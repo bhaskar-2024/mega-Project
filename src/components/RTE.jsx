1d7507413df475ec5322cad1ba5ebdf17a85717a
Bhaskar Mishra <93215023+bhaskar-2024@users.noreply.github.com>
2024-04-16 00:26:12 +0530

    Update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,7 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-        apiKey='9q4zr31h9whqadn46wwxp1hi038iunizk84zyua44fmingnd'
+        apiKey=''
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
